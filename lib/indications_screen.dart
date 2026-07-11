261ec7a1398e58be57cef9f105c7bf095ff66d19
Abdus <abdusalam@MacBook-Air-Abdus.local>
2026-07-11 22:50:14 +0600

    Добавил диалог с показаниями

--- lib/indications_screen.dart
+++ lib/indications_screen.dart
@@ -7,9 +7,77 @@ class IndicationsScreen extends StatefulWidget {
   State<StatefulWidget> createState() => _State();
 }
 
-class _State extends State<IndicationsScreen>{
+class _State extends State<IndicationsScreen> {
+  void _addDialog(BuildContext context) {
+    DateTime selectedDate = DateTime.now();
+    final TextEditingController controller = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setStateDialog) {
+            return AlertDialog(
+              title: Text('Добавление показания'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '${selectedDate.day}.${selectedDate.month}.${selectedDate.year}',
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          final picker = await showDatePicker(
+                            context: context,
+                            initialDate: selectedDate,
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2099),
+                          );
+                          if (picker != null) {
+                            setStateDialog(() {
+                              selectedDate = picker;
+                            });
+                          }
+                        },
+                        icon: Icon(Icons.calendar_month),
+                      ),
+                    ],
+                  ),
+                  TextField(
+                    controller: controller,
+                    decoration: InputDecoration(hintText: 'новые показания'),
+                    keyboardType: TextInputType.number,
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('отмена'),
+                ),
+                TextButton(onPressed: () {}, child: Text('добавить')),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(widget.runtimeType.toString()),);
+    return Scaffold(
+      body: Center(child: Text(widget.runtimeType.toString())),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
