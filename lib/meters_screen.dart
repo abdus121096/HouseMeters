b56bfe530e035edafa6aa9c22bf821084c6192fb
Abdus <abdusalam@MacBook-Air-Abdus.local>
2026-07-19 20:52:53 +0600

    Добавлено редактирование

--- lib/meters_screen.dart
+++ lib/meters_screen.dart
@@ -3,8 +3,9 @@ import 'package:house_meters/indications_screen.dart';
 
 class Meter {
   String name;
+  int intValue;
 
-  Meter(this.name);
+  Meter(this.name, this.intValue);
 }
 
 class MetersScreen extends StatefulWidget {
@@ -16,9 +17,9 @@ class MetersScreen extends StatefulWidget {
 
 class _MetersScreenState extends State<MetersScreen> {
   final List<Meter> meter = [
-    Meter('Электричество'),
-    Meter('Холодная вода'),
-    Meter('Горячая вода'),
+    Meter('Электричество', 0),
+    Meter('Холодная вода', 0),
+    Meter('Горячая вода', 0),
   ];
 
   void _addDialog(BuildContext context) {
@@ -44,12 +45,76 @@ class _MetersScreenState extends State<MetersScreen> {
             ],
           ),
           actions: [
-            TextButton(onPressed: () {
-              Navigator.of(context).pop();
-            }, child: Text('отмена')),
-            TextButton(onPressed: () {
-              Navigator.of(context).pop();
-            }, child: Text('сохранить')),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('отмена'),
+            ),
+            TextButton(
+              onPressed: () {
+                final name = controller1.text;
+                final value = int.tryParse(controller2.text);
+                if (name.isNotEmpty && value != null) {
+                  setState(() {
+                    meter.add(Meter(name, value));
+                  });
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text('сохранить'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _editDialog(BuildContext context, int index) {
+    final item = meter[index];
+    final TextEditingController controller3 = TextEditingController(text: item.name);
+    final TextEditingController controller4 = TextEditingController(text: item.intValue.toString());
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Изменения счетчика'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(controller: controller3),
+              TextField(controller: controller4, enabled: false),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  meter.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('удалить'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('отмена'),
+            ),
+            TextButton(
+              onPressed: () {
+                final name2 = controller3.text;
+                if (name2.isNotEmpty) {
+                  setState(() {
+                    meter[index] = Meter(name2, item.intValue);
+                  });
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text('сохранить'),
+            ),
           ],
         );
       },
@@ -69,9 +134,18 @@ class _MetersScreenState extends State<MetersScreen> {
         itemCount: meter.length,
         itemBuilder: (context, index) {
           final item = meter[index];
-          return ListTile(title: Text(item.name), onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => IndicationsScreen()));
-          },);
+          return ListTile(
+            title: Text(item.name),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => IndicationsScreen()),
+              );
+            },
+            onLongPress: () {
+              _editDialog(context, index);
+            },
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
